da8742cabdc8831471e409e74fc5774bd5538ab5
Eden Candelas <e.candelas@gmail.com>
2012-10-09 19:35:21 -0500

    log lines added and fixed the readings.append()

--- server.py
+++ server.py
@@ -10,13 +10,16 @@ class cologger(SocketServer.BaseRequestHandler):
     def handle(self):
         reading = '0'
         data = self.request[0].strip().split(":")
+        print "monitor: " + data[1]
+        print "reading: " + data[0]
         db = shelve.open(FILE)
         if not 'readings' in db:
             db['readings'] = []
         readings = db['readings']
 
         if data[0] >= 0 and len(data) > 1:
-            readings.append(data[1], data[0])
+            readings.append(data[1])
+            readings.append(data[0])
 	    
         db['readings'] = readings
         db.close()
